1ea162f14c5b493e9051cd910186e903cb30a30f
sooyeon <tndus77@sookmyung.ac.kr>
2021-07-11 00:13:59 +0900

    KakaoLogin 화면 구현 -3

--- app/src/main/java/com/example/sjapp/LoginActivity.java
+++ app/src/main/java/com/example/sjapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.sjapp;
 import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
